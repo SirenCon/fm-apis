ce8f682d36bc7b009859cc90a651ac47f193db59
Matt Ullman <static@staticfox.net>
2025-02-17 01:32:42 +0000

    SC_FALSE birthday disclaimer

--- registration/context_processors.py
+++ registration/context_processors.py
@@ -3,3 +3,11 @@ from django.conf import settings
 
 def square_environment(request):
     return {"SQUARE_ENVIRONMENT": settings.SQUARE_ENVIRONMENT}
+
+
+def sirencon(request):
+    # Easy way to grep through things we've disabled that need to be
+    # factored in to upstream.
+    return {
+        "SC_FALSE": False,
+    }
